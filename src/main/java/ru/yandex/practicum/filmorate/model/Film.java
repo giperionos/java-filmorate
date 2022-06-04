8e97c2692e1d57311a50e85f66230e20145955cb
Alexander Tolstykh <tolstikh-aleksandr@yandex.ru>
2022-06-05 00:54:44 +0300

    fix: Исправить дефект валидации для фильма

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,9 +3,10 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Data;
 import ru.yandex.practicum.filmorate.validators.DateNotEarlier;
 
-import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Data
@@ -14,9 +15,10 @@ public class Film {
     private Long id;
 
     @NotNull(message = "Не указано название фильма.")
+    @NotBlank(message = "Указано пустое название фильма.")
     private String name;
 
-    @Max(value = 200, message = "К фильму дано слишком длинное описание.")
+    @Size(max = 200, message = "К фильму дано слишком длинное описание.")
     private String description;
 
     @DateNotEarlier(value = "28.12.1895", message = "Указана некорректная дата релиза.")
