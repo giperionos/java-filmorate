4d4440fad5d59fa5c1b5f3d79ab01b8ff022bd6b
Tolstykh Alexander <tolstikh-aleksandr@yandex.ru>
2022-06-04 13:16:19 +0300

    feat: Добавить в модель данных классы Film и User

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.filmorate.model;
+
+import java.time.LocalDateTime;
+
+public class Film {
+
+    private Integer id;
+    private String name;
+    private String description;
+    private LocalDateTime releaseDate;
+    private Integer duration;
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.filmorate.model;
+
+import java.time.LocalDateTime;
+
+public class User {
+
+    private Integer id;
+    private String email;
+    private String login;
+    private String name;
+    private LocalDateTime birthday;
+}
