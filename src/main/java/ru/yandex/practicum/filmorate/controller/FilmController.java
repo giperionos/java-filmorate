81b93be83021b4dbec5d71827cea08ba4eb90f41
Alexander Tolstykh <tolstikh-aleksandr@yandex.ru>
2022-06-05 23:04:24 +0300

    feat: Добавить логирование операций, которые изменяют сущности

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exceptions.UnknownFilmException;
 import ru.yandex.practicum.filmorate.model.Film;
@@ -13,38 +14,50 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/films")
+@Slf4j
 public class FilmController {
 
     private Map<Long, Film> films = new HashMap<>();
 
     @PostMapping
     public Film add(@Valid @RequestBody Film film) {
+        log.debug("Обработка POST запроса по пути /films на добавление фильма.");
+        log.debug("Пришел объект: " + film);
 
         //установить новый id для нового фильма
         film.setId(IdSequence.getNewFilmId());
+        log.debug("Фильму присвоен id = " + film.getId());
 
         //сохранить новый фильм
         films.put(film.getId(), film);
+        log.debug("Фильм c id = " + film.getId() + " сохранен.");
 
         //и вернуть его
+        log.debug("Фильм: " + film + " передан клиенту.");
         return films.get(film.getId());
     }
 
     @PutMapping
     public Film update(@Valid @RequestBody Film film) {
+        log.debug("Обработка PUT запроса по пути /films на обновление фильма.");
+        log.debug("Пришел объект: " + film);
 
         //поискать фильм для обновления
+        log.debug("Поиск фильма с id = " + film.getId() + " в памяти приложения.");
         Film updatedFilm = films.get(film.getId());
 
         //если нет такого - ошибка
         if (updatedFilm == null) {
+            log.debug("Ошибка поиска фильма.");
             throw new UnknownFilmException(String.format("Фильм с %d не найден!", film.getId()));
         }
 
         //обновить фильм
         films.put(film.getId(), film);
+        log.debug("Фильм c id = " + film.getId() + " обновлен.");
 
         //и вернуть его
+        log.debug("Фильм: " + film + " передан клиенту.");
         return films.get(film.getId());
     }
 
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exceptions.UnknownUserException;
 import ru.yandex.practicum.filmorate.model.User;
@@ -13,33 +14,43 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
+@Slf4j
 public class UserController {
     private Map<Long, User> users = new HashMap<>();
 
     @PostMapping
     public User add(@Valid @RequestBody User user) {
+        log.debug("Обработка POST запроса по пути /users на добавление пользователя.");
+        log.debug("Пришел объект: " + user);
 
         //установить новый id для нового пользователя
         user.setId(IdSequence.getNewUserId());
+        log.debug("Пользователю присвоен id = " + user.getId());
 
         //уточнить имя пользователя - если не задано, установить значением из login
         defineUserName(user);
 
         //сохранить нового пользователя
         users.put(user.getId(), user);
+        log.debug("Пользователь c id = " + user.getId() + " сохранен.");
 
         //и вернуть его
+        log.debug("Пользователь: " + user + " передан клиенту.");
         return users.get(user.getId());
     }
 
     @PutMapping
     public User update(@Valid @RequestBody User user) {
+        log.debug("Обработка PUT запроса по пути /users на обновление пользователя.");
+        log.debug("Пришел объект: " + user);
 
         //поискать пользователя для обновления
+        log.debug("Поиск пользователя с id = " + user.getId() + " в памяти приложения.");
         User updatedUser = users.get(user.getId());
 
         //если нет такого - ошибка
         if (updatedUser == null) {
+            log.debug("Ошибка поиска пользователя.");
             throw new UnknownUserException(String.format("Пользователь с %d не найден!", user.getId()));
         }
 
@@ -48,8 +59,10 @@ public class UserController {
 
         //обновить пользователя
         users.put(user.getId(), user);
+        log.debug("Пользователь c id = " + user.getId() + " обновлен.");
 
         //и вернуть его
+        log.debug("Пользователь: " + user + " передан клиенту.");
         return users.get(user.getId());
     }
 
@@ -60,8 +73,11 @@ public class UserController {
 
     //уточнить имя пользователя - если не задано, установить значением из login
     private void defineUserName(User user){
+        log.debug("Проверка имени пользователя с id = " + user.getId());
         if (user.getName().isEmpty() || user.getName().isBlank()) {
+            log.debug("У пользователя не задано имя.");
             user.setName(user.getLogin());
+            log.debug("Пользователю с id = " + user.getId() + " присвоено имя: " + user.getName());
         }
     }
 }
